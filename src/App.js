b44d9d93f208b1a874eb117cf72a26e99b11dfbc
DeveloperVasu <81623637+DeveloperVasu@users.noreply.github.com>
2022-03-10 22:27:58 +0530

    Created a list using map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class  App extends Component {
+    constructor() {
+        super();
+        this.state = {
+           frontend: [
+               {
+                   name: 'Angular',
+                   version: '13.1.1'
+               },
+               {
+                   name: 'React',
+                   version: '17.0.2'
+               },
+               {
+                   name: 'Vue',
+                   version: '3.2.23'
+               },
+               {
+                   name: 'Ember',
+                   version: '1.13.11'
+               },
+               {
+                   name: 'Backbone',
+                   version: '1.3.3'
+               }
+           ]
+        };
+    }
+
+  render() {
+    return <div className='App'>
+        {this.state.frontend.map((framework) => <h1>{framework.name.toUpperCase()}</h1>)}
     </div>
-  );
+  }
 }
 
 export default App;
